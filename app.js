ca3320ad11d922c698ea11759feffef50b0f538e
danny235 <danielbarima235@gmail.com>
2020-08-24 21:20:35 +0100

    update on time

--- app.js
+++ app.js
@@ -4,60 +4,65 @@ let type1 = document.querySelector(".type1");
 let type2 = document.querySelector(".type2");
 let display = document.querySelector("h1");
 
-document.addEventListener("onload", showTime());
 
-// Time Getter
-function showTime() {
+document.addEventListener("DOMContentLoaded", milTime);
+// Show military time
+function milTime() {
+
+    clearTimeout(window.htime);
+    var date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    // logic
+    m = m < 10 ? m = `0${m}` : m;
+    s = s < 10 ? s = `0${s}` : s;
+
+    // time output
+    let time = `${h}:${m}:${s}`;
+    mTime = setTimeout(milTime, 1000);
+    display.innerHTML = time;
+    type2.classList.remove('active');
     type1.className += " active";
-    var date = new Date();    
-    var h, m, s, time;
-    h = date.getHours();
-    m = date.getMinutes();
-    s = date.getSeconds();
+
+
+
+}
+
+
+// Show 12 hour time
+function hourTime() {
+
+    clearTimeout(mTime);
+    var date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
     session = "AM";
-
-    if(h == 0) {
-        h = 12;
-    }
-
+    // logic
     if (h > 12) {
         h -= 12;
         session = "PM"
     }
-
-    if (h < 10) {
-        h = `0${h}`
-    }
-
-    if (m < 10) {
-        m = `0${m}`
-    }
-
-    if (s < 10) {
-        s = `0${s}`
-    }
-
-
-    // type1.addEventListener('click', function(){
-    //     type1.className += " active";
-    //     type2.classList.remove("active");
-    // });
     
-    // type2.addEventListener('click', function(){
-    //     type2.className += " active";
-    //     type1.classList.remove("active");
-    // });
+    m = m < 10 ? m = `0${m}` : m;
+    s = s < 10 ? s = `0${s}` : s;
+    window.htime = setTimeout(hourTime, 1000);
+    let time = `${h}:${m}:${s} ${session}`;
 
-
-    time = `${h}:${m}:${s} ${session}`;
     display.innerHTML = time;
-    setTimeout(showTime, 1000);
-    
+    type1.classList.remove('active');
+    type2.className += " active";
+
+
 }
-showTime();
+type1.addEventListener("click", milTime);
+type2.addEventListener("click", hourTime);
+
 
 // looping through the days of the week
-for(let i = 0, max = dayOfWeek.length; i < max; i++) {
+for (let i = 0, max = dayOfWeek.length; i < max; i++) {
     var date = new Date();
     var day = date.getDay();
     var screenWidth = window.innerWidth;
@@ -70,7 +75,59 @@ for(let i = 0, max = dayOfWeek.length; i < max; i++) {
         dayOfWeek[i].style.display = "inline-block";
     }
 
-    
+
 }
+
+// // Time Getter
+// function showTime() {
+//     type1.className += " active";
+//     var date = new Date();    
+//     var h, m, s, time;
+//     h = date.getHours();
+//     m = date.getMinutes();
+//     s = date.getSeconds();
+//     session = "AM";
+
+//     if(h == 0) {
+//         h = 12;
+//     }
+
+//     if (h > 12) {
+//         h -= 12;
+//         session = "PM"
+//     }
+
+//     if (h < 10) {
+//         h = `0${h}`
+//     }
+
+//     if (m < 10) {
+//         m = `0${m}`
+//     }
+
+//     if (s < 10) {
+//         s = `0${s}`
+//     }
+
+
+//     // type1.addEventListener('click', function(){
+//     //     type1.className += " active";
+//     //     type2.classList.remove("active");
+//     // });
+
+//     // type2.addEventListener('click', function(){
+//     //     type2.className += " active";
+//     //     type1.classList.remove("active");
+//     // });
+
+
+//     time = `${h}:${m}:${s} ${session}`;
+//     display.innerHTML = time;
+//     setTimeout(showTime, 1000);
+
+// }
+
+
+
 
 
