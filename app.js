54699e1cd53879e18e24e33f6ce4c5283dbc871a
danny235 <danielbarima235@gmail.com>
2020-09-24 09:50:37 +0100

    debugging the js code

--- app.js
+++ app.js
@@ -3,40 +3,40 @@ let type1 = document.querySelector(".type1");
 let type2 = document.querySelector(".type2");
 let display = document.querySelector("h1");
 
-
+// EventListeners
 document.addEventListener("DOMContentLoaded", milTime);
+type1.addEventListener("click", milTime);
+type2.addEventListener("click", hourTime);
+
+
+
 // Show military time
 function milTime() {
-
     // clearing the hourTime
     clearTimeout(window.htime);
-    function createMiltimeObject() {
-        // declaration
-        var date = new Date();
-        let h = date.getHours();
-        let m = date.getMinutes();
-        let s = date.getSeconds();
-
-        // logic
-        h = h < 10 ? h = `0${h}` : h;
-        m = m < 10 ? m = `0${m}` : m;
-        s = s < 10 ? s = `0${s}` : s;
-        // time output
-        let time = `${h}:${m}:${s}`;
-        display.innerHTML = time;
-        mTime = setTimeout(createMiltimeObject, 1000);
-
-    }
-
-
-
     createMiltimeObject();
 
-
+    // add and remove class from buttons
     type2.classList.remove('active');
     type1.className += " active";
+}
 
+// creating the military time object
+function createMiltimeObject() {
+    // declaration
+    var date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
 
+    // logic
+    h = h < 10 ? h = `0${h}` : h;
+    m = m < 10 ? m = `0${m}` : m;
+    s = s < 10 ? s = `0${s}` : s;
+    // time output
+    let time = `${h}:${m}:${s}`;
+    display.innerHTML = time;
+    mTime = setTimeout(createMiltimeObject, 1000);
 
 }
 
@@ -47,45 +47,47 @@ function hourTime() {
     // clearing the militaryTime
     clearTimeout(mTime);
 
-    function createHourTime() {
-        // declaration
-        var date = new Date();
-        let h = date.getHours();
-        let m = date.getMinutes();
-        let s = date.getSeconds();
-        session = "AM";
-
-        // logic
-        if (h > 12) {
-            h -= 12;
-        }
-
-        if (h <= 12) {
-            session = "PM"
-        }
-        h = h == 0 ? h = 12 : h;
-        h = h < 10 ? h = `0${h}` : h;
-        m = m < 10 ? m = `0${m}` : m;
-        s = s < 10 ? s = `0${s}` : s;
-
-        // make this global so it can be accessed by militaryTime
-        window.htime = setTimeout(createHourTime, 1000);
-
-        // time output
-        let time = `${h}:${m}:${s} ${session}`;
-        display.innerHTML = time;
-    }
-
     createHourTime();
 
-
+    // add and remove active class from buttons
     type1.classList.remove('active');
     type2.className += " active";
 
 
 }
-type1.addEventListener("click", milTime);
-type2.addEventListener("click", hourTime);
+
+// Creating the hour time object
+function createHourTime() {
+    // declaration
+    var date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    session = "AM";
+
+    // logic
+    if (h > 12) {
+        h -= 12;
+    }
+
+    if (h <= 12) {
+        session = "PM"
+    }
+    h = h == 0 ? h = 12 : h;
+    h = h < 10 ? h = `0${h}` : h;
+    m = m < 10 ? m = `0${m}` : m;
+    s = s < 10 ? s = `0${s}` : s;
+
+    // make this global so it can be accessed by militaryTime
+    window.htime = setTimeout(createHourTime, 1000);
+
+    // time output
+    let time = `${h}:${m}:${s} ${session}`;
+    display.innerHTML = time;
+}
+
+
+
 
 
 // looping through the days of the week
